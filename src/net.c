3a2e2f855383c27aca45e0c64a7f683d7f7f0243
MrBouzaffour <ahmedbouzaffour2002@gmail.com>
2025-12-08 19:38:48 -0600

    [Implementation] implemented udp_create_socket

--- src/net.c
+++ src/net.c
@@ -1,1 +1,19 @@
-/* Author: Ahmed Bouzaffour 2025-12-08*/
+/* Author: Ahmed Bouzaffour 2025-12-08
+ * Creating a UDP socket
+*/
+
+#include "common.h"
+
+int
+udp_create_socket()
+{
+	int sockfd;
+
+	sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+	if (sockfd < 0)
+	{
+		perror("[ERROR] Socket creation failed");
+		return -1;
+	}
+	return sockfd;
+}
